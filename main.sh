d5fbd47863272da3445579b9571182a2ad2e28d1
Marco Stuurman <marco@fe.ax>
2024-08-08 08:55:25 +0200

    Retry flow after lockup

--- main.sh
+++ main.sh
@@ -22,40 +22,60 @@ MAX_RETRIES=10
 RETRY_COUNT=0
 
 while true; do
-  curl -s -X POST \
-    -H "Authorization: token $AUTH_TOKEN" \
-    -H "Content-Type: application/json" \
-    -d '{"name":"Zerotier GitHub Member '"${GITHUB_SHA::7}"'", "description": "Member created by '"${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"'", "config":{"authorized":true}}' \
-    "$API_URL/network/$NETWORK_ID/member/${member_id}"
 
-  sleep 1
+  while true; do
+    curl -s -X POST \
+      -H "Authorization: token $AUTH_TOKEN" \
+      -H "Content-Type: application/json" \
+      -d '{"name":"Zerotier GitHub Member '"${GITHUB_SHA::7}"'", "description": "Member created by '"${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"'", "config":{"authorized":true}}' \
+      "$API_URL/network/$NETWORK_ID/member/${member_id}"
 
-  curl -s -X GET \
-    -H "Authorization: token $AUTH_TOKEN" \
-    -H "Content-Type: application/json" \
-    "$API_URL/network/$NETWORK_ID/member/${member_id}" | jq '.config.authorized' -r | grep true && break;
+    sleep 1
 
-  RETRY_COUNT=$((RETRY_COUNT + 1))
+    curl -s -X GET \
+      -H "Authorization: token $AUTH_TOKEN" \
+      -H "Content-Type: application/json" \
+      "$API_URL/network/$NETWORK_ID/member/${member_id}" | jq '.config.authorized' -r | grep true && break;
 
-  if [ $RETRY_COUNT -ge $MAX_RETRIES ]; then
-    echo "Reached maximum number of retries ($MAX_RETRIES). Exiting..."
-    exit 1
-  fi
+    RETRY_COUNT=$((RETRY_COUNT + 1))
 
-  echo "Authorization failed. Retrying in 2 seconds... (Attempt $RETRY_COUNT of $MAX_RETRIES)"
-  sleep 2
+    if [ $RETRY_COUNT -ge $MAX_RETRIES ]; then
+      echo "Reached maximum number of retries ($MAX_RETRIES). Exiting..."
+      exit 1
+    fi
+
+    echo "Authorization failed. Retrying in 2 seconds... (Attempt $RETRY_COUNT of $MAX_RETRIES)"
+    sleep 2
+  done
+
+  echo "Member authorized successfully."
+  echo "⏁  Joining ZeroTier Network ID: $NETWORK_ID"
+  case $(uname -s) in
+  MINGW64_NT?*)
+    "${ztcli}" join $NETWORK_ID
+    while ! "${ztcli}" listnetworks | grep $NETWORK_ID | grep OK; do sleep 0.5; done
+    ;;
+  *)
+    sudo zerotier-cli join $NETWORK_ID
+    MAX_ITERATIONS=10
+    ITERATION=0
+
+    while true; do
+      if sudo zerotier-cli listnetworks | grep $NETWORK_ID | grep OK; then
+        break 2
+      fi
+
+      ITERATION=$((ITERATION + 1))
+
+      if [ $ITERATION -ge $MAX_ITERATIONS ]; then
+        echo "Reached maximum number of iterations ($MAX_ITERATIONS). Exiting..."
+        continue 2
+      fi
+
+      echo "Network not yet joined. Retrying in 0.5 seconds... (Iteration $ITERATION of $MAX_ITERATIONS)"
+      sleep 0.5
+    done
+    ;;
+  esac
+
 done
-
-echo "Member authorized successfully."
-echo "⏁  Joining ZeroTier Network ID: $NETWORK_ID"
-case $(uname -s) in
-MINGW64_NT?*)
-  "${ztcli}" join $NETWORK_ID
-  while ! "${ztcli}" listnetworks | grep $NETWORK_ID | grep OK; do sleep 0.5; done
-  ;;
-*)
-  sudo zerotier-cli join $NETWORK_ID
-  while ! sudo zerotier-cli listnetworks | grep $NETWORK_ID | grep OK; do sleep 0.5; done
-  ;;
-esac
-
