cf9d86e9a0ca8316395ce81e2a7ecc547e30c82c
Marco Stuurman <marco@fe.ax>
2024-09-01 20:38:56 +0200

    Speed up installation ZeroTier

--- util/install.sh
+++ util/install.sh
@@ -1,4 +1,7 @@
-wget https://install.zerotier.com/ -O install.sh
-chmod +x install.sh
-sed -i '2iset -x' install.sh
-sudo bash install.sh
+#!/bin/bash
+
+dpkg -i zerotier-one_1.4.6_amd64.deb
+/etc/init.d/zerotier-one start
+while [ ! -f /var/lib/zerotier-one/identity.secret ]; do
+	sleep 1
+done
