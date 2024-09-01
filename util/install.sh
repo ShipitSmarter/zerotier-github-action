85930e831ae1f8a268befba8b1808e475d223f40
Marco Stuurman <marco@fe.ax>
2024-09-01 21:25:36 +0200

    Speed up installation ZeroTier

--- util/install.sh
+++ util/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo dpkg -i $GITHUB_ACTION_PATH/util/zerotier-one_1.4.6_amd64.deb
+sudo dpkg -i $GITHUB_ACTION_PATH/util/zerotier-one_1.14.0_amd64.deb
 sudo /etc/init.d/zerotier-one start
 while [ ! -f /var/lib/zerotier-one/identity.secret ]; do
 	sleep 1
