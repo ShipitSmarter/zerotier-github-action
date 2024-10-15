ce8e5a2d5ace532933e7d3d34284d8a37d2e39b1
Marco Stuurman <marco@fe.ax>
2024-10-15 13:54:12 +0200

    Add arm64 support

--- util/install.sh
+++ util/install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-sudo dpkg -i $GITHUB_ACTION_PATH/util/zerotier-one_1.14.0_amd64.deb
+
+ARCH=$(uname -m)
+
+if [[ "$ARCH" == "x86_64" ]]; then
+    TARGET="amd64"
+elif [[ "$ARCH" == "arm64" ]]; then
+    TARGET="arm64"
+elif [[ "$ARCH" == "aarch64" ]]; then
+    TARGET="arm64"
+else
+    echo "Unknown architecture: $ARCH"
+		exit 1
+fi
+
+
+sudo dpkg -i $GITHUB_ACTION_PATH/util/zerotier-one_1.14.0_${TARGET}.deb
 sudo /etc/init.d/zerotier-one start
 while [ ! -f /var/lib/zerotier-one/identity.secret ]; do
 	sleep 1
